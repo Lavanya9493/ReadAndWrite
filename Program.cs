7527abc3068b71d3b501a84d6d5a6b19bf92383d
Lavanya9493 <lavanyajonnalagadda123@gmail.com>
2021-11-11 15:53:14 +0530

    Name

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReadAndWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+            string name=Console.ReadLine();
+            Console.WriteLine("Hello"+name);
+            Console.WriteLine("Hello{0}",name);
+            Console.WriteLine($"Hello{name}");
+        }
+    }
+}
